fe496f263b82a7b072a97e1ba89d381c8a1b2995
WhitneySt <whitneystena418@gmail.com>
2022-12-13 17:02:43 -0500

    Agregando ejemplos de la sesión 4 de bucles y condicionales

---
+++ sprint1/bucles-condicionales/script1.js
@@ -0,0 +1,21 @@
+//Ejemplo con la estructura cíclica while
+
+let n = 0;
+let x = 0;
+
+while (n < 4) {
+  n++;
+  x += n;
+  console.log("Cuando n = ", n, " x = ", x);
+}
+
+//Ejemplo con la estructura cíclica for
+
+for (let i = 0; i < 10; i++) {
+  //Aqui van todas las lineas de intrucciones que se va ejecutar, siempre que la condición evaluada sea true
+}
+
+for (let i = 0; i <= 10; i++) {
+    document.write(i);
+    document.write("<br>");    
+}
