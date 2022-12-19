41cef89cd978020af52dc77ed5f7ce2df437ada5
WhitneySt <whitneystena418@gmail.com>
2022-12-19 09:10:46 -0500

    Agregando ejercicios de la sesión 7

---
+++ sprint1/eventos-dom/script.js
@@ -0,0 +1,64 @@
+//Ejercicio consiste en crear una página que nos permita filtrar personajes de acuerdo a ciertas características. Este filtro lo vamos a realizar con unos botones de filtrado.
+
+//1. Definir el arrays de personajes (Star wars)
+
+const starWars = [
+  {
+    name: "Luke Skywalker",
+    height: "172",
+    mass: "77",
+    hairColor: "blond",
+    skinColor: "fair",
+    eyeColor: "blue",
+    birthYear: "19BBY",
+    gender: "male",
+    image: "https://smoda.elpais.com/wp-content/uploads/2019/12/2-look.jpg",
+  },
+  {
+    name: "C-3PO",
+    height: "167",
+    mass: "75",
+    hairColor: "n/a",
+    skinColor: "gold",
+    eyeColor: "yellow",
+    birthYear: "112BBY",
+    gender: "n/a",
+    image: "https://upload.wikimedia.org/wikipedia/en/5/5c/C-3PO_droid.png",
+  },
+  {
+    name: "R2-D2",
+    height: "96",
+    mass: "32",
+    hairColor: "n/a",
+    skinColor: "white, blue",
+    eyeColor: "red",
+    birthYear: "33BBY",
+    gender: "n/a",
+    image:
+      "https://static.wikia.nocookie.net/esstarwars/images/e/e2/Artoo-Fathead.png/revision/latest?cb=20180108172244",
+  },
+  {
+    name: "Darth Vader",
+    height: "202",
+    mass: "136",
+    hairColor: "none",
+    skinColor: "white",
+    eyeColor: "yellow",
+    birthYear: "41.9BBY",
+    gender: "male",
+    image:
+      "https://media.revistagq.com/photos/62a0a996223a33e985e4d59a/master/pass/1072434_110615-cc-Darth-Vader-Thumb.jpg",
+  },
+  {
+    name: "Leia Organa",
+    height: "150",
+    mass: "49",
+    hairColor: "brown",
+    skinColor: "light",
+    eyeColor: "brown",
+    birthYear: "19BBY",
+    gender: "female",
+    image:
+      "https://static.wikia.nocookie.net/esstarwars/images/f/ff/Leia_photomasher.jpg/revision/latest?cb=20081221003327",
+  },
+];
